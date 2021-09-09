3f38034df8a28b8d1e8c5d75dd9b89eeb2e98c25
Adnan-Swati <79781004+Adnan-Swati@users.noreply.github.com>
2021-09-09 05:34:49 -0700

    Add files via upload

---
+++ marksheet.dart
@@ -0,0 +1,34 @@
+void main() {
+ 
+  
+var a="Mark Sheet";
+  print(a);
+  
+  
+String b = "Muhammad Adnan";
+  print ("Name          :   ${b}");
+String c = "Muhammad Mumtaz";    
+ print ("Fathe Name    :   ${c}"); 
+  String m = "class         :   12th";
+  print (m);
+String  d ="D J Sindh Government college";
+  print ( "Centre Name   :   ${d}" ); 
+  
+  
+  String e = "S.No      Subjects       Max Marks   Marks Obtained   Result";
+  print (e);
+  var f =   "1          Mathes          100         80              Pass";
+  print (f);
+  var g =   "2          Physics         100         80              pass";
+  print (g);
+  var h =   "3          Chemistry       100         80              Pass";
+  print (h);
+  var i =   "4          Biology         100         80              pass";
+  print (i);
+  var j =   "5          Urdu            100         80              pass";
+  print (j);
+  var k =   "6          English         100         80              Pass";
+  print (k);
+  var l =  "           Grand Total     600         480             80%";
+  print (l);
+}
